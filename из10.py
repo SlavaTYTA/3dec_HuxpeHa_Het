b1db440b1d62ecf012b4ab2222a7c9e9f389ed2b
Ты здесь не найдёшь имени <114632474+SlavaTYTA@users.noreply.github.com>
2022-10-22 13:50:53 +0700

    Update and rename Перевод из 10.py to из10.py

--- из10.py
+++ из10.py
@@ -1,9 +1,9 @@
 N10=int(input("N10="))
 p=int(input("p="))
-k=1
-Np=0
-while N10!=0:
+k=int(1)
+Np=int(0)
+while not N10==0:
     Np=Np+(N10%p)*k
     k=k*10
     N10=N10//p
-print(f"N{p} = {Np} " )
+print(f"Np={Np}")

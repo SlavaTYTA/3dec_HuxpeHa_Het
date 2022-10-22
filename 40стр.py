97ef5e7926d69e0f0601d2aa53ad46cf0c9dd83a
Ты здесь не найдёшь имени <114632474+SlavaTYTA@users.noreply.github.com>
2022-10-22 14:02:53 +0700

    Create 40стр.py

---
+++ 40стр.py
@@ -0,0 +1,13 @@
+
+p = int(input("введите основание системы, p = "))
+Np = int(input(f"Исходное число N{p} = "))
+k = int(1)
+N10 = int(0)
+
+while not Np==0:
+    N10 = N10 + (Np % 10) * k
+    k = k * p
+    Np = Np // 10
+print("N10 = ", N10)
+Нижний колонтитул
+© 2022 GitHub, Inc.
